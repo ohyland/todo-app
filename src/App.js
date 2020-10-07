cf674a4253420413a43d7f98e93ae15ea38986ad
ohyland <52823456+ohyland@users.noreply.github.com>
2020-10-07 12:10:14 +0100

    add-comments

--- src/App.js
+++ src/App.js
@@ -1,49 +1,119 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
-
 import "./style.css";
 import TodoItem from "./TodoItem";
 
-const TodoItemsFromOutSide = [
-  { id: "learn-react", labelName: "Learn React" },
-  { id: "create-todo-app", labelName: "Create a todo app" },
-  { id: "profit", labelName: "Profit" },
-  { id: "have-fun", labelName: "Have fun!" },
-  { id: "prettier", labelName: "Prettier is awesome!" },
-];
+const TodoItemsFromOutSide = [];
 
 const App = () => {
   const [todos, setTodos] = useState(TodoItemsFromOutSide);
   const [newTodo, setNewTodo] = useState("");
 
+  // useEffect(()=> {
+  //   const newTodos = JSON.parse(localStorage.getItem("todos") || "[]");
+  //   setTodos(newTodos)
+  // }, []); // render once
+
+  // useEffect(()=> {
+  //   localStorage.getItem("todos") || "[]");
+  //   setTodos(newTodos)
+  // }, []); // render once
+
+  // useEffect(()=> {
+  //   localStorage.getItem("todos") || "[]");
+  //   setTodos(newTodos)
+  // }, []); // render once
+  
   const handleSubmit = (e) => {
-    e.preventDefault();
+    
+    e.preventDefault(); // stops page refreshing on submit
     const newTodos = [
       ...todos,
-      { id: newTodo.replace(" ", "-"), labelName: newTodo },
+      { id: newTodo.replace(" ", "-"), labelName: newTodo, completed: false },
     ];
     setTodos(newTodos);
-    setNewTodo("");
+    setNewTodo("");  // rest input to empty string
+    // localStorage.setItem("todos", JSON.stringify(newTodos))
   };
+  // click checkbox to  change state from original state to oppisite
+  const updateTodo = (id) => {
+    console.log("this is todos before updating", todos);
+    // map through each item in current to do list
+    const newTodos = todos.map((item) => {
+      if (item.id === id) {
+        console.log("Yay, found the todo");
+        return { ...item, completed: !item.completed };
+      }
+      return item;
 
+    });
+    console.log("This is the todos after updating", newTodos);
+    setTodos(newTodos);
+  };
+  const handleTodoChange
+
+  //renders in the dom
   return (
-    <div id="my-todo-app" className="my-todo-app">
-      <h1>My todo app</h1>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <input
-          value={newTodo}
-          onChange={(e) => {
-            setNewTodo(e.target.value);
-          }}
-        />
-      </form>
-      {todos.map((item) => {
-        return (
-          <TodoItem key={item.id} id={item.id} labelName={item.labelName} />
-        );
-      })}
+    // JSX
+    <div className="todoapp">
+      <header className="header">
+        <h1>My Todo List</h1>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input
+            className="new-todo"
+            placeholder="What do you need to do today?"
+            value={newTodo}
+            onChange={(e) => {
+              setNewTodo(e.target.value);
+            }}
+          />
+        </form>
+      </header>
+      <section className="main">
+        <ul className="todo-list">
+          {todos.map((item) => {
+            return (
+              <TodoItem
+                key={item.id} // identifier for react to know this item only is updated instead of going through the whole list
+                id={item.id}
+                labelName={item.labelName}
+                completed={item.completed}
+                updateTodo={updateTodo}
+                handleChange={updateTodo}
+                // handleDelete={deleteTodo}
+              />
+            );
+          })}
+        </ul>
+      </section>
     </div>
   );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+/*
+
+    Wed 7 Oct
+    ---------
+    save data so when refresh page it doesnt
+    - - -
+    use browser storage - local storage
+    window.localStorage localStorage
+    Google how local storage works .. Mozilla Firefox
+    localStorage.setItem
+    localStorage.getItem
+    using localStorage and JSON
+    filter 
+
+    new hook useEffect
+      - - -
+      useEffect(()=> {
+
+      })
+
+    install react hook
+    - - -
+
+
+*/
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -4,18 +4,33 @@ import PropTypes from "prop-types";
 
 export const myClassName = "todo-item";
 
-const TodoItem = ({ id, labelName }) => {
+const TodoItem = ({ id, labelName, completed, handleChange }) => {
+  // const props = {id: "learn", labelName: "Learn"}
+  // props = properties of the component
+  //handleChange should handle my input chnages
+
   return (
-    <div className={myClassName}>
-      <input type="checkbox" id={id} />
-      <label htmlFor={id}>{labelName}</label>
-    </div>
+    <li>
+      <div className={"view"}>
+        <input
+          type="checkbox"
+          id={id}
+          className={"toggle"}
+          checked={completed}
+          onChange={(e) => handleChange(id)}
+        />
+        <label htmlFor={id}>{labelName}</label>
+        {/* <button className="destroy" onClick={handleDelete}> */}
+      </div>
+    </li>
   );
 };
 
 TodoItem.propTypes = {
-  id: PropTypes.string,
-  labelName: PropTypes.string,
+  id: PropTypes.string.isRequired,
+  labelName: PropTypes.string.isRequired,
+  completed: PropTypes.bool.isRequired,
+  handleUpdates: PropTypes.func.isRequired,
 };
 
 export default TodoItem;
