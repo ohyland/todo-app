bc6fc16edfba901e35ee7902a255cac3d9d890c3
ohyland <52823456+ohyland@users.noreply.github.com>
2020-10-07 12:14:36 +0100

    Add handler function to update a single todo

--- src/App.js
+++ src/App.js
@@ -1,67 +1,42 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
+
 import "./style.css";
 import TodoItem from "./TodoItem";
 
-const TodoItemsFromOutSide = [];
-
 const App = () => {
-  const [todos, setTodos] = useState(TodoItemsFromOutSide);
+  const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState("");
 
-  // useEffect(()=> {
-  //   const newTodos = JSON.parse(localStorage.getItem("todos") || "[]");
-  //   setTodos(newTodos)
-  // }, []); // render once
-
-  // useEffect(()=> {
-  //   localStorage.getItem("todos") || "[]");
-  //   setTodos(newTodos)
-  // }, []); // render once
-
-  // useEffect(()=> {
-  //   localStorage.getItem("todos") || "[]");
-  //   setTodos(newTodos)
-  // }, []); // render once
-  
   const handleSubmit = (e) => {
-    
-    e.preventDefault(); // stops page refreshing on submit
+    e.preventDefault();
     const newTodos = [
       ...todos,
       { id: newTodo.replace(" ", "-"), labelName: newTodo, completed: false },
     ];
     setTodos(newTodos);
-    setNewTodo("");  // rest input to empty string
-    // localStorage.setItem("todos", JSON.stringify(newTodos))
+    setNewTodo("");
   };
-  // click checkbox to  change state from original state to oppisite
+
   const updateTodo = (id) => {
-    console.log("this is todos before updating", todos);
-    // map through each item in current to do list
     const newTodos = todos.map((item) => {
-      if (item.id === id) {
-        console.log("Yay, found the todo");
+      if (item.id == id) {
         return { ...item, completed: !item.completed };
       }
       return item;
-
     });
-    console.log("This is the todos after updating", newTodos);
+
     setTodos(newTodos);
   };
-  const handleTodoChange
 
-  //renders in the dom
   return (
-    // JSX
     <div className="todoapp">
       <header className="header">
-        <h1>My Todo List</h1>
+        <h1>Todos</h1>
         <form onSubmit={(e) => handleSubmit(e)}>
           <input
             className="new-todo"
-            placeholder="What do you need to do today?"
+            placeholder="What needs to be done?"
             value={newTodo}
             onChange={(e) => {
               setNewTodo(e.target.value);
@@ -74,13 +49,11 @@ const App = () => {
           {todos.map((item) => {
             return (
               <TodoItem
-                key={item.id} // identifier for react to know this item only is updated instead of going through the whole list
+                key={item.id}
                 id={item.id}
                 labelName={item.labelName}
                 completed={item.completed}
-                updateTodo={updateTodo}
                 handleChange={updateTodo}
-                // handleDelete={deleteTodo}
               />
             );
           })}
@@ -91,29 +64,3 @@ const App = () => {
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-/*
-
-    Wed 7 Oct
-    ---------
-    save data so when refresh page it doesnt
-    - - -
-    use browser storage - local storage
-    window.localStorage localStorage
-    Google how local storage works .. Mozilla Firefox
-    localStorage.setItem
-    localStorage.getItem
-    using localStorage and JSON
-    filter 
-
-    new hook useEffect
-      - - -
-      useEffect(()=> {
-
-      })
-
-    install react hook
-    - - -
-
-
-*/
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,25 +2,18 @@ import React from "react";
 
 import PropTypes from "prop-types";
 
-export const myClassName = "todo-item";
-
 const TodoItem = ({ id, labelName, completed, handleChange }) => {
-  // const props = {id: "learn", labelName: "Learn"}
-  // props = properties of the component
-  //handleChange should handle my input chnages
-
   return (
-    <li>
-      <div className={"view"}>
+    <li className={completed ? "completed" : ""}>
+      <div className="view">
         <input
           type="checkbox"
           id={id}
-          className={"toggle"}
+          className="toggle"
           checked={completed}
-          onChange={(e) => handleChange(id)}
+          onChange={() => handleChange(id)}
         />
         <label htmlFor={id}>{labelName}</label>
-        {/* <button className="destroy" onClick={handleDelete}> */}
       </div>
     </li>
   );
@@ -30,7 +23,7 @@ TodoItem.propTypes = {
   id: PropTypes.string.isRequired,
   labelName: PropTypes.string.isRequired,
   completed: PropTypes.bool.isRequired,
-  handleUpdates: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
 };
 
 export default TodoItem;
