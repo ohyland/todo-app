da2eff925e983017a670b879a697d2788723fcce
ohyland <52823456+ohyland@users.noreply.github.com>
2020-10-12 11:32:18 +0100

    Add use effect to set/get todos and new-todo to/from local storage

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
 import "./style.css";
 import TodoItem from "./TodoItem";
 
-const myTodos = localStorage.getItem("todos") || "[]";
-
 // APP
 const App = () => {
-  const [todos, setTodos] = useState(JSON.parse(myTodos));
-  // [newTodo, setNewTodo] passed into the componant
-  const [newTodo, setNewTodo] = useState(""); // useState - react hook to set state for componant
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    const newTodos = JSON.parse(localStorage.getItem("todos") || "[]");
+    setTodos(newTodos);
+
+    let newTodoFromStorage = localStorage.getItem("new-todo");
+    if (newTodoFromStorage) {
+      newTodoFromStorage = JSON.parse(newTodoFromStorage);
+    } else {
+      newTodoFromStorage = "";
+    }
+
+    setNewTodo(newTodoFromStorage);
+  }, []);
+
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  useEffect(() => {
+    localStorage.setItem("new-todo", JSON.stringify(newTodo));
+  }, [newTodo]);
 
   const handleSubmit = (e) => {
     e.preventDefault(); // prevent page refresh on submit
     const newTodos = [
-      ...todos,
       // id - replace spaces in the text with "-" from the labelName
       // completed - controlles whether the item is checked
+      ...todos,
       { id: newTodo.replace(" ", "-"), labelName: newTodo, completed: false },
     ];
     setTodos(newTodos);
-    setNewTodo(""); // reset to ""
-    localStorage.setItem("todos", JSON.stringify(newTodos));
+    setNewTodo(""); // reset
   };
+
 
   // change val
   // when checkbox is cliked the id of the item is passed in
@@ -37,22 +57,13 @@ const App = () => {
       }
       return item;
     });
-
     setTodos(newTodos); // put todo in the todo list
     // reset todos
-    localStorage.setItem("todos", JSON.stringify(newTodos));
   };
 
-  // click X and delete the item
   const deleteTodo = (id) => {
-    // filter - filters out anything that doesn't match the criteria
-    // eg:  > const myNumbers = [1, 2 , 3, 4, 5]
-    //      myNumbers.filter(num => num !== 2)
-    //      > [1, 2 , 3, 4, 5]
-    //      
     const newTodos = todos.filter((todo) => todo.id !== id);
     setTodos(newTodos);
-    localStorage.setItem("todos", JSON.stringify(newTodos));
   };
 
   // rendered in the DOM
@@ -60,9 +71,7 @@ const App = () => {
     <div className="todoapp">
       <header className="header">
         <h1>Todos</h1>
-        {/* form to handle form submission */}
         <form onSubmit={(e) => handleSubmit(e)}>
-          {/* new todo input */}
           <input
             className="new-todo"
             placeholder="What needs to be done?"
@@ -78,7 +87,7 @@ const App = () => {
           {todos.map((item) => {
             return (
               <TodoItem
-                key={item.id} // update what has changed
+                key={item.id}
                 id={item.id}
                 labelName={item.labelName}
                 completed={item.completed}
