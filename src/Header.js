b1291b5993a0881e27a2ac6bce97d1d86992e3ce
Olivia Hyland <52823456+ohyland@users.noreply.github.com>
2020-10-13 11:22:35 +0100

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 const Header = ({handleSubmit, newTodo, handleChange }) => {
     return (
         <header className="header">
-            <h1>Olivias Todos</h1>
+            <h1>Olivia\'s Todos</h1>
             <form onSubmit={handleSubmit}>
             <input
                 className="new-todo"
